55bf25bafe427079e2c7f355fa488072e848366c
Zegmundo - G7 <zegmundo.koziel@hotmail.com>
2022-09-20 21:50:47 -0300

    atualização de codigos js forca

--- forca.js
+++ forca.js
@@ -2,6 +2,24 @@
 let palavras = ["ALURA", "ORACLE", "FORCA", "HTML", "JAVASCRIPT", "PROGRAMADOR", "LOGICA", "FRONTAND"];
 let tabuleiro = document.getElementById("forca").getContext('2d');
 let palavraSecreta = "";
+let novaPalavraSecreta = document.getElementById('salvar-iniciar');
+let inputPalavra = document.querySelector("#input-nova-palavra")
+let letrasClicadas = [];
+
+
+
+function addNovaPalavra(){
+	if (palavras.includes(inputPalavra.value.toUpperCase())){
+		document.querySelector("#input-nova-palavra").value="";
+		iniciarJogo();
+		} else{
+			palavras.push(inputPalavra.value.toUpperCase());
+    		document.querySelector("#input-nova-palavra").value="";
+    		console.log(palavras);
+    		iniciarJogo();
+
+			}    
+}
 
 function escolherPalavraSecreta(){
 	let palavra = palavras[Math.floor(Math.random() * palavras.length)];
@@ -16,6 +34,7 @@ function iniciarJogo(){
 	escolherPalavraSecreta();
 	desenharCanvas();
 	desenharLinhas();
+	letrasClicadas = [];
 }
 function novaPalavra() {
 	document.getElementById('div-desaparece').style.display = "none";
@@ -28,8 +47,22 @@ function recarregar(){
 	document.getElementById('div-desaparece').style.display = "block";
 }
 
+
 recarregar();
 
 function teste(){
 alert ("clicou");
 }
+
+function teclaClicada(){
+	if (letrasClicadas.includes(event.key.toUpperCase())){
+		} else{
+			letrasClicadas.push(event.key.toUpperCase());
+    		console.log(letrasClicadas);
+    			}    
+}
+
+
+document.body.onkeypress = teclaClicada;
+  
+
